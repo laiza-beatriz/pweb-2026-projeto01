b24c58a3f044bff6940a2548cbed111d132aca51
Laiza Beatriz <laizabeatriz517@gmail.com>
2026-05-23 16:50:01 -0300

    projeto finalizado

--- meuapp/views.py
+++ meuapp/views.py
@@ -8,7 +8,88 @@ def inicio(request):
     return render(request, 'inicio.html')
 
 def equipe(request):
-    return render(request, 'equipe.html')
+    jogador = [
+        {
+            'nome': 'Hinata',
+            'posicao': 'Central',
+            'idade': '16 anos',
+        },
+
+        {
+            'nome': 'Kageyama',
+            'posicao': 'Levantador',
+            'idade': '16 anos',
+        },
+
+        {
+            'nome': 'Yamaguchi',
+            'posicao': 'Central',
+            'idade': '16 anos',
+        },
+
+        {
+            'nome': 'Daichi',
+            'posicao': 'Ponteiro',
+            'idade': '18 anos',
+        },
+
+        {
+            'nome': 'Nishinoya',
+            'posicao': 'Líbero',
+            'idade': '16 anos',
+        },
+
+        {
+            'nome': 'Tsukishima',
+            'posicao': 'Central',
+            'idade': '16 anos',
+        },
+
+        {
+            'nome': 'Asahi',
+            'posicao': 'Ponteiro',
+            'idade': '18 anos',
+        },
+
+        {
+            'nome': 'Tanaka',
+            'posicao': 'Ponteiro',
+            'idade': '18 anos',
+        },
+    ]
+
+    context = {
+        'jogador': jogador
+    }
+
+    return render(request, 'equipe.html', context)
+
 
 def sobre(request):
-    return render(request, 'sobre.html')
+
+    alunos = [
+        {
+            'nome': 'Laíza',
+            'curso': 'Técnica em infoweb',
+            'idade': '17 anos',
+        },
+
+        {
+            'nome': 'Samuel',
+            'curso': 'Técnico em infoweb',
+            'idade': '17 anos',
+        }
+    ]
+
+    descricao = """
+    Somos alunos do IFRNSPP cursando Informática para internet.
+    Escolhemos esse anime Haikyuu! para fazer o projeto da matéria
+    programação web, onde apresentamos a equipe Karasuno.
+    """
+
+    context = {
+        'alunos': alunos,
+        'descricao': descricao
+    }
+
+    return render(request, 'sobre.html', context)
